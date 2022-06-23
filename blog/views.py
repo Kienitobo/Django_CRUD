730411eb7ed972888e36de9feb7234293065e377
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2022-06-23 23:49:36 +0100

    blog views.py modified

--- blog/views.py
+++ blog/views.py
@@ -10,10 +10,10 @@ class PostCreateView(generic.CreateView):
     fields="__all__"
     success_url=reverse_lazy("blog:all")
 
-    class PostDetailView(generic.DetailView):
+class PostDetailView(generic.DetailView):
         model=Post
 
-    class PostDeleteView(generic.DeleteView):
+class PostDeleteView(generic.DeleteView):
         model=Post
         fields="__all__"
         success_url=reverse_lazy("blog:all")
